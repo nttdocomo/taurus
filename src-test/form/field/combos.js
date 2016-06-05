5622af4035264aff86b99e611dfa367cd785bfcc
nttdocomo <nttdocomo.ouyi@gmail.com>
2016-06-05 22:17:41 +0800

    update

--- src-test/form/field/combos.js
+++ src-test/form/field/combos.js
@@ -5,13 +5,13 @@
             // Now we're wrapping the factory and assigning the return
             // value to the root (window) and returning it as well to
             // the AMD loader.
-            define(['../../../src/backbone','../../../src/classic/form/field/comboBox'],function(Base,StoreHolder){
+            define(['../../../src/backbone','../../../src/underscore','../../../src/classic/form/field/comboBox'],function(Base,StoreHolder){
               return (root.Class = factory(Base,StoreHolder));
             });
         }
         if(define.cmd){
             define(function(require, exports, module){
-                return (root.Class = factory(require('../../../src/backbone'),require('../../../src/classic/form/field/comboBox')));
+                return (root.Class = factory(require('../../../src/backbone'),require('../../../src/underscore'),require('../../../src/classic/form/field/comboBox')));
             })
         }
     } else if(typeof module === "object" && module.exports) {
@@ -19,11 +19,11 @@
         // run into a scenario where plain modules depend on CommonJS
         // *and* I happen to be loading in a CJS browser environment
         // but I'm including it for the sake of being thorough
-        module.exports = (root.Class = factory(require('../../../src/backbone'),require('../../../src/classic/form/field/comboBox')));
+        module.exports = (root.Class = factory(require('../../../src/backbone'),require('../../../src/underscore'),require('../../../src/classic/form/field/comboBox')));
     } else {
         root.Class = factory();
     }
-}(this, function(Backbone,ComboBox) {
+}(this, function(Backbone,_,ComboBox) {
     var run = function() {
 		QUnit.test("ComboBox", function( assert ) {
 			assert.expect( 6 );
@@ -161,6 +161,63 @@
 			assert.equal(comboBox.value, '2', "当点击下拉框第三个选项时" );
 			//comboBox.getPicker().remove();
 			//comboBox.remove();
+		});
+		QUnit.test("MultiSelect ComboBox", function( assert ) {
+			var collection = new Backbone.Collection([{
+				name:'aaaa',
+				value:'0'
+			},{
+				name:'bbbb',
+				value:'1'
+			},{
+				name:'aabb',
+				value:'2'
+			}]),
+			$el = $('<div></div>'),
+			comboBox = new ComboBox({
+				queryDelay:1,
+				renderTo:$el,
+				emptyText:'emptyText',
+				displayField : 'name',
+				valueField:'value',
+				queryMode : 'local',
+				collection : collection,
+				multiSelect:true
+			}),
+			event = $.Event('click',{
+				target : comboBox.triggerEl.get(0)
+			}),
+			picker,
+			listEvent;
+			comboBox.$el.trigger(event);
+			assert.equal(comboBox.getPicker().$el.is(':hidden'), false, "当点击下拉按钮时，显示下拉框" );
+			comboBox.$el.trigger(event);
+			assert.equal(comboBox.getPicker().$el.is(':hidden'), true, "当点击下拉按钮时，隐藏下拉框" );
+			comboBox.$el.trigger(event);
+			assert.equal(comboBox.getPicker().$el.is(':hidden'), false, "当点击下拉按钮时，显示下拉框" );
+			picker = comboBox.getPicker();
+			listEvent = $.Event('click',{
+				target : picker.$el.find('a').eq(0).get(0)
+			});
+			comboBox.getPicker().$el.trigger(listEvent);
+			listEvent = $.Event('click',{
+				target : picker.$el.find('a').eq(1).get(0)
+			});
+			comboBox.getPicker().$el.trigger(listEvent);
+			assert.ok(_.isEqual(comboBox.value, ['0','1']), "当点击下拉框第一，二个选项时" );
+			assert.equal(comboBox.getPicker().$el.is(':hidden'), false, "多选时下拉框不收起" );//因为是多选，所以在点击下拉框元素进行选择时下拉框不会收起
+			listEvent = $.Event('click',{
+				target : picker.$el.find('a').eq(1).get(0)
+			});
+			comboBox.getPicker().$el.trigger(listEvent);
+			assert.ok(_.isEqual(comboBox.value, ['0']), "当再次点击下拉框第一个选项时，取消选择" );
+			listEvent = $.Event('click',{
+				target : picker.$el.find('a').eq(0).get(0)
+			});
+			comboBox.getPicker().$el.trigger(listEvent);
+			assert.ok(_.isEqual(comboBox.value, []), "当再次点击下拉框第一个选项时，取消选择，值为空数组" );
+			comboBox.getPicker().remove();
+			comboBox.remove();
 		});
     };
     return {run: run}
