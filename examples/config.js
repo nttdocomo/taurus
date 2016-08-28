0e4db443f6f06da5cff8ae8c50a1c6a814fac500
nttdocomo <nttdocomo.ouyi@gmail.com>
2016-08-28 21:04:11 +0800

    add highlight.js

--- examples/config.js
+++ examples/config.js
@@ -1,18 +1,19 @@
 /**
  * @author nttdocomo
  */
+ /* global location*/
 seajs.config({
-	alias : {
-	    'button':'taurus/button/button',
-	    //'backbone-pageable':'../../src/backbone-pageable'/*,
-	    //'underscore':'../../src/backbone-pageable'
-	},
-	paths: {
-		'taurus':'./'
-	},
-	base : location.pathname.replace(/^(\/taurus)?\/.*/,'$1') + '/src',
-	charset : 'utf-8',
-	vars : {
-		'locale' : (navigator.language || navigator.browserLanguage).toLowerCase()
-	}
-});
+  alias: {
+    'button': 'taurus/button/button'
+  // 'backbone-pageable':'../../src/backbone-pageable'/*,
+  // 'underscore':'../../src/backbone-pageable'
+  },
+  paths: {
+    'taurus': './'
+  },
+  base: location.pathname.replace(/^(\/taurus)?\/.*/, '$1') + '/src',
+  charset: 'utf-8',
+  vars: {
+    'locale': (navigator.language || navigator.browserLanguage).toLowerCase()
+  }
+})
