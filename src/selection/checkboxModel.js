81041bee168ea25762b37e11725473ec0c97a731
nttdocomo <nttdocomo.ouyi@gmail.com>
2016-11-30 23:17:30 +0800

    update

--- src/selection/checkboxModel.js
+++ src/selection/checkboxModel.js
@@ -3,26 +3,27 @@
     // Now we're wrapping the factory and assigning the return
     // value to the root (window) and returning it as well to
     // the AMD loader.
-    define(['./model', '../classic/grid/column/column', 'taurus'], function (Backbone) {
+    define(['./model', '../classic/grid/column/column', 'underscore', 'taurus'], function (Backbone) {
       return (root.Class = factory(Backbone))
     })
   }
   if (define.cmd) {
     define(function (require, exports, module) {
-      return (root.Class = factory(require('./model'), require('../classic/grid/column/column'), require('taurus')))
+      return (root.Class = factory(require('./model'), require('../classic/grid/column/column'), require('underscore'), require('taurus')))
     })
   } else if (typeof module === 'object' && module.exports) {
     // I've not encountered a need for this yet, since I haven't
     // run into a scenario where plain modules depend on CommonJS
     // *and* I happen to be loading in a CJS browser environment
     // but I'm including it for the sake of being thorough
-    module.exports = (root.Class = factory(require('./model'), require('../classic/grid/column/column'), require('taurus')))
+    module.exports = (root.Class = factory(require('./model'), require('../classic/grid/column/column'), require('underscore'), require('taurus')))
   } else {
     root.Class = factory()
   }
-}(this, function (Model, Column, taurus) {
+}(this, function (Model, Column, _, taurus) {
   var CheckboxModel = Model.extend({
     injectCheckbox: 0,
+    headerWidth: 32,
     constructor: function () {
       var me = this
       me._super.apply(me, arguments)
--- src/view/abstractView.js
+++ src/view/abstractView.js
@@ -1,57 +1,65 @@
-(function (root, factory) {
-    if(typeof define === "function"){
-        if(define.amd) {
-            // Now we're wrapping the factory and assigning the return
-            // value to the root (window) and returning it as well to
-            // the AMD loader.
-            define(['./base','../util/storeHolder','underscore'],function(Base,StoreHolder,_){
-              return (root.Class = factory(Base,StoreHolder));
-            });
-        }
-        if(define.cmd){
-            define(function(require, exports, module){
-                return (root.Class = factory(require('./base'),require('../util/storeHolder'),require('underscore')));
-            })
-        }
-    } else if(typeof module === "object" && module.exports) {
-        // I've not encountered a need for this yet, since I haven't
-        // run into a scenario where plain modules depend on CommonJS
-        // *and* I happen to be loading in a CJS browser environment
-        // but I'm including it for the sake of being thorough
-        module.exports = (root.Class = factory(require('./base'),require('../util/storeHolder'),require('underscore')));
-    } else {
-        root.Class = factory();
+;(function (root, factory) {
+  if (typeof define === 'function') {
+    if (define.amd) {
+      // Now we're wrapping the factory and assigning the return
+      // value to the root (window) and returning it as well to
+      // the AMD loader.
+      define(['./base', '../util/storeHolder', 'underscore'], function (Base, StoreHolder, _) {
+        return (root.Class = factory(Base, StoreHolder))
+      })
     }
-}(this, function(Base,StoreHolder,_) {
-	return Base.extend({
-		initComponent:function(){
-			var me = this;
-			if (!me.itemSelector) {
-                me.itemSelector = '.' + me.itemCls;
-            }
-            Base.prototype.initComponent.apply(this,arguments);
-            me.bindStoreListeners(me.collection)
-		},
-
-	    getStoreListeners: function() {
-	        var me = this;
-	        return {
-	            refresh: me.onDataRefresh,
-	            replace: me.onReplace,
-	            /*add: me.onAdd,*/
-	            remove: _.debounce(me.onRemove, 200),
-	            change: me.onUpdate,
-              update: me.onAdd,
-	            clear: me.onDataRefresh,
-	            beginupdate: me.onBeginUpdate,
-	            endupdate: me.onEndUpdate
-	        };
-	    },
-	    onUpdate:function(store, record, operation, modifiedFieldNames, details){
-	    	console.log(arguments)
-	    },
-      onRemove:function(){
-	    	console.log(arguments)
+    if (define.cmd) {
+      define(function (require, exports, module) {
+        return (root.Class = factory(require('./base'), require('../util/storeHolder'), require('underscore')))
+      })
+    }
+  } else if (typeof module === 'object' && module.exports) {
+    // I've not encountered a need for this yet, since I haven't
+    // run into a scenario where plain modules depend on CommonJS
+    // *and* I happen to be loading in a CJS browser environment
+    // but I'm including it for the sake of being thorough
+    module.exports = (root.Class = factory(require('./base'), require('../util/storeHolder'), require('underscore')))
+  } else {
+    root.Class = factory()
+  }
+}(this, function (Base, StoreHolder, _) {
+  return Base.extend({
+    initComponent: function () {
+      var me = this
+      if (!me.itemSelector) {
+        me.itemSelector = '.' + me.itemCls
       }
-	}).mixins(StoreHolder)
+      Base.prototype.initComponent.apply(this, arguments)
+      me.bindStoreListeners(me.collection)
+      me.getNavigationModel().bindComponent(this)
+    },
+
+    getStoreListeners: function () {
+      var me = this
+      return {
+        refresh: me.onDataRefresh,
+        replace: me.onReplace,
+        /*add: me.onAdd,*/
+        remove: _.debounce(me.onRemove, 200),
+        change: me.onUpdate,
+        update: me.onAdd,
+        clear: me.onDataRefresh,
+        beginupdate: me.onBeginUpdate,
+        endupdate: me.onEndUpdate
+      }
+    },
+    onUpdate: function (store, record, operation, modifiedFieldNames, details) {
+      console.log(arguments)
+    },
+    onRemove: function () {
+      console.log(arguments)
+    },
+
+    applyNavigationModel: function (navigationModel) {
+      return Ext.Factory.viewNavigation(navigationModel)
+    },
+    getNavigationModel: function () {
+      return this.navigationModel
+    }
+  }).mixins(StoreHolder)
 }))
