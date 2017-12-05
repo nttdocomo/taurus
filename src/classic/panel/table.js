36ce7ada6238ce9ce9c7ac7f88884f922553019b
ouyi <ouyi@didichuxing.com>
2017-12-05 16:09:24 +0800

    update

--- src/classic/panel/table.js
+++ src/classic/panel/table.js
@@ -4,15 +4,15 @@
  (function (root, factory) {
 	if(typeof define === "function") {
 		if(define.amd){
-			define(['../../mixin/mix','./panel','./mixins','../view/table','../grid/header/container','../grid/pagination','../grid/navigationModel', 'backbone.paginator','underscore','../../taurus'], factory);
+			define(['../../mixin/mix','./panel','./mixins','../view/table','../grid/header/container','../grid/pagination','../grid/navigationModel', 'backbone.paginator','underscore','taurus'], factory);
 		}
 		if(define.cmd){
 			define(function(require, exports, module){
-				return factory(require('../../mixin/mix'),require('./panel'),require('./mixins'),require('../view/table'),require('../grid/header/container'),require('../grid/pagination'),require('../grid/navigationModel'),require('backbone.paginator'),require('underscore'),require('../../taurus'));
+				return factory(require('../../mixin/mix'),require('./panel'),require('./mixins'),require('../view/table'),require('../grid/header/container'),require('../grid/pagination'),require('../grid/navigationModel'),require('backbone.paginator'),require('underscore'),require('taurus'));
 			})
 		}
 	} else if(typeof module === "object" && module.exports) {
-		module.exports = factory(require('../../mixin/mix'),require('./panel'),require('./mixins'),require('../view/table'),require('../grid/header/container'),require('../grid/pagination'),require('../grid/navigationModel'),require('backbone.paginator'),require('underscore'),require('../../taurus'));
+		module.exports = factory(require('../../mixin/mix'),require('./panel'),require('./mixins'),require('../view/table'),require('../grid/header/container'),require('../grid/pagination'),require('../grid/navigationModel'),require('backbone.paginator'),require('underscore'),require('taurus'));
 	}
 }(this, function(mix, Panel,mixins,Table,Header,Pagination,NavigationModel,PageableCollection, _,taurus){
 	return mix(Panel).with(mixins).extend({
