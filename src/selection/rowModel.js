41adb9c831ca8a74c56b0fd5b872faf1109054d9
ouyi <ouyi@didichuxing.com>
2017-12-11 14:22:50 +0800

    update

--- src/selection/rowModel.js
+++ src/selection/rowModel.js
@@ -4,11 +4,11 @@
     // value to the root (window) and returning it as well to
     // the AMD loader.
     if (define.amd) {
-      define(['./dataViewModel', '../classic/grid/column/column', 'underscore', '../taurus'], factory)
+      define(['./dataViewModel', '../classic/grid/column/column', 'underscore', 'taurus'], factory)
     }
     if (define.cmd) {
       define(function (require, exports, module) {
-        return (root.Class = factory(require('./dataViewModel'), require('../classic/grid/column/column'), require('underscore'), require('../taurus')))
+        return (root.Class = factory(require('./dataViewModel'), require('../classic/grid/column/column'), require('underscore'), require('taurus')))
       })
     }
   } else if (typeof module === 'object' && module.exports) {
@@ -16,7 +16,7 @@
     // run into a scenario where plain modules depend on CommonJS
     // *and* I happen to be loading in a CJS browser environment
     // but I'm including it for the sake of being thorough
-    module.exports = (root.Class = factory(require('./dataViewModel'), require('../classic/grid/column/column'), require('underscore'), require('../taurus')))
+    module.exports = (root.Class = factory(require('./dataViewModel'), require('../classic/grid/column/column'), require('underscore'), require('taurus')))
   } else {
     root.Class = factory()
   }
