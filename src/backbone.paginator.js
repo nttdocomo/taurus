68483c8a49e177c3dc39f7e39f42b02e78e3841a
ouyi <ouyi@didichuxing.com>
2017-12-27 14:37:59 +0800

    update

--- src/backbone.paginator.js
+++ src/backbone.paginator.js
@@ -79,7 +79,7 @@
 
   function finiteInt (val, name) {
     if (!_.isNumber(val) || _.isNaN(val) || !_.isFinite(val) || ~~val !== val) {
-      throw new TypeError("`" + name + "` must be a finite integer");
+      throw new TypeError("`" + name + "` must be a finite integer, got `" + val + "`");
     }
     return val;
   }
